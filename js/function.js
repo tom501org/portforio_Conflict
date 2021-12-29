e9f60f501d21750d08a446a77e3187c41a053a22
koichi4226 <91336573+tom501org@users.noreply.github.com>
2021-12-30 03:54:42 +0900

    調整

--- js/function.js
+++ js/function.js
@@ -1,52 +1,52 @@
-$(function(){
-  // ここにjQueryを記述
-  //ハンバーガー
-  $('#btn').on('click',function(){
-    $('#btn__top').toggleClass('rotate-top')
-    // $('#btn__middle').toggleClass('hide-middle')
-    $('#btn__bottom').toggleClass('rotate-bottom')
-  $('#gnav').slideToggle();
-  })
+$(function() {
+    // ここにjQueryを記述
+    //ハンバーガー
+    $('#btn').on('click', function() {
+        $('#btn__top').toggleClass('rotate-top')
+            // $('#btn__middle').toggleClass('hide-middle')
+        $('#btn__bottom').toggleClass('rotate-bottom')
+        $('#gnav').slideToggle();
+    })
 })
 
 
-$(function(){
-  
-  //カーソル要素の指定
-  let cursor=$("#cursor");
-  //ちょっと遅れてついてくるストーカー要素の指定  
-  let stalker=$("#stalker");
-  
-  //mousemoveイベントでカーソル要素を移動させる
-  $(document).on("mousemove",function(e){
-    //カーソルの座標位置を取得
-    let x=e.clientX;
-    let y=e.clientY;
-    //カーソル要素のcssを書き換える用
-    cursor.css({
-      "opacity":"1",
-      "top":y+"px",
-      "left":x+"px"
-    });
-    //ストーカー要素のcssを書き換える用    
-    setTimeout(function(){
-      stalker.css({
-        "opacity":"1",
-        "top":y+"px",
-        "left":x+"px"
-      });
-    },5);//カーソルより遅れる時間を指定
-    
-  });
+$(function() {
 
-  $(document).ready(function(){
-    $('#btn').on('click',function(){
-      $('#gnav').toggleClass('open');
-      $('body').toggleClass('noscroll');
+    //カーソル要素の指定
+    let cursor = $("#cursor");
+    //ちょっと遅れてついてくるストーカー要素の指定  
+    let stalker = $("#stalker");
+
+    //mousemoveイベントでカーソル要素を移動させる
+    $(document).on("mousemove", function(e) {
+        //カーソルの座標位置を取得
+        let x = e.clientX;
+        let y = e.clientY;
+        //カーソル要素のcssを書き換える用
+        cursor.css({
+            "opacity": "1",
+            "top": y + "px",
+            "left": x + "px"
+        });
+        //ストーカー要素のcssを書き換える用    
+        setTimeout(function() {
+            stalker.css({
+                "opacity": "1",
+                "top": y + "px",
+                "left": x + "px"
+            });
+        }, 5); //カーソルより遅れる時間を指定
+
     });
-  });
+
+    $(document).ready(function() {
+        $('#btn').on('click', function() {
+            $('#gnav').toggleClass('open');
+            $('body').toggleClass('noscroll');
+        });
+    });
 
 
 
 });
-  // --------------
+// --------------
