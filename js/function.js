d64f311b8338509db4070a1fdaed2c8fac78c5a6
tom501org <koichi4226@gmail.com>
2022-01-11 14:20:20 +0900

    ローディング追加

--- js/function.js
+++ js/function.js
@@ -59,3 +59,22 @@ window.addEventListener('load', function() {
         observer.unobserve(e);
     }
 }, false);
+
+
+
+//読み込みが完了したら実行
+$(window).on('load',function () {
+  // ローディングが10秒以内で終わる場合、読み込み完了後ローディング非表示
+  endLoading();
+});
+
+//10秒経過した段階で、上記の処理を上書き、強制終了
+setTimeout('endLoading()', 10000);
+
+//ローディング非表示処理
+function endLoading(){
+  // 1秒かけてロゴを非表示にし、その後0.8秒かけて背景を非表示にする
+  $('.js-loading p').fadeOut(2500, function(){
+    $('.js-loading').fadeOut(500);
+  });
+}
