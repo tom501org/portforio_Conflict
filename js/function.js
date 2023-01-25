a3e5ead1f133fef7a1632fd855fa70950ec99377
501org <91336573+tom501org@users.noreply.github.com>
2023-01-26 07:32:50 +0900

    ooo

--- js/function.js
+++ js/function.js
@@ -45,61 +45,61 @@ window.addEventListener('load', function() {
 
 
 
-// $(function() {
-//     let stageW = 0; // 画面の幅
-//     let stageH = 0; // 画面の高さ
-//     // canvas要素の参照を取得
-//     const canvas = document.querySelector('.loop');
-//     // 2Dの描画命令群を取得
-//     const context = canvas.getContext('2d');
-//     noise.seed(Math.random());
-//     resize();
-//     tick();
-//     window.addEventListener('resize', resize);
-//     /** エンターフレームのタイミングです。 */
-//     function tick() {
-//         requestAnimationFrame(tick);
-//         const time = Date.now() / 10000;
-//         draw(time);
-//     }
-//     /** 描画します。 */
-//     function draw(time) {
-//         // 画面をリセット
-//         context.clearRect(0, 0, stageW, stageH);
-//         context.lineWidth = 1;
-//         const amplitude = stageH / 2; // 振幅（縦幅)の大きさ
-//         const lineNum = 50; // ラインの数
-//         const segmentNum = 100; // 分割数
-//         [...Array(lineNum).keys()].forEach(j => {
-//             const coefficient = 50 + j;
-//             context.beginPath();
-//             const h = Math.round(j / lineNum * 360); // 色相
-//             const s = 100; // 彩度
-//             const l = Math.round(j / lineNum * 100); // 明度
-//             context.strokeStyle = `hsl(${h}, ${s}%, ${l}%)`;
-//             [...Array(segmentNum).keys()].forEach(i => {
-//                 const x = i / (segmentNum - 1) * stageW;
-//                 const px = i / coefficient;
-//                 const py = (j / 100 + time);
-//                 const y = amplitude * noise.perlin2(px, py) + stageH / 2;
-//                 if (i === 0) {
-//                     context.moveTo(x, y);
-//                 } else {
-//                     context.lineTo(x, y);
-//                 }
-//             });
-//             context.stroke();
-//         });
-//     }
-//     /** リサイズ時のイベントです。 */
-//     function resize() {
-//         stageW = innerWidth * devicePixelRatio;
-//         stageH = innerHeight * devicePixelRatio;
+$(function() {
+    let stageW = 0; // 画面の幅
+    let stageH = 0; // 画面の高さ
+    // canvas要素の参照を取得
+    const canvas = document.querySelector('.loop');
+    // 2Dの描画命令群を取得
+    const context = canvas.getContext('2d');
+    noise.seed(Math.random());
+    resize();
+    tick();
+    window.addEventListener('resize', resize);
+    /** エンターフレームのタイミングです。 */
+    function tick() {
+        requestAnimationFrame(tick);
+        const time = Date.now() / 10000;
+        draw(time);
+    }
+    /** 描画します。 */
+    function draw(time) {
+        // 画面をリセット
+        context.clearRect(0, 0, stageW, stageH);
+        context.lineWidth = 1;
+        const amplitude = stageH / 2; // 振幅（縦幅)の大きさ
+        const lineNum = 50; // ラインの数
+        const segmentNum = 100; // 分割数
+        [...Array(lineNum).keys()].forEach(j => {
+            const coefficient = 50 + j;
+            context.beginPath();
+            const h = Math.round(j / lineNum * 360); // 色相
+            const s = 100; // 彩度
+            const l = Math.round(j / lineNum * 100); // 明度
+            context.strokeStyle = `hsl(${h}, ${s}%, ${l}%)`;
+            [...Array(segmentNum).keys()].forEach(i => {
+                const x = i / (segmentNum - 1) * stageW;
+                const px = i / coefficient;
+                const py = (j / 100 + time);
+                const y = amplitude * noise.perlin2(px, py) + stageH / 2;
+                if (i === 0) {
+                    context.moveTo(x, y);
+                } else {
+                    context.lineTo(x, y);
+                }
+            });
+            context.stroke();
+        });
+    }
+    /** リサイズ時のイベントです。 */
+    function resize() {
+        stageW = innerWidth * devicePixelRatio;
+        stageH = innerHeight * devicePixelRatio;
 
-//         canvas.width = stageW;
-//         canvas.height = stageH;
-//     }
-// });
+        canvas.width = stageW;
+        canvas.height = stageH;
+    }
+});
 
 
 
